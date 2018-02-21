7860a58838e79e81c5b6a956aa0b134cb33cbd32
Unknown <real83@hotmail.fr>
2018-02-22 00:10:30 +0100

    Avancée de la page Results
    Avancée majeure de la page results (.html et .ts)

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -22,9 +22,9 @@ export class ResultsPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     Array.from(this.params = navParams.get("firstPassed"));
 
-    
+
     console.log(this.params);
-  /*   this.getJourney(); */
+    /*   this.getJourney(); */
   }
 
   ionViewDidLoad() {
@@ -38,5 +38,186 @@ export class ResultsPage {
 
   hack(val) {
     return Array.from(val);
+  }
+  timeDisplayDeparture(string) {
+    var day: String;
+    var month: String;
+    var year: String;
+    var hour: String;
+    var minutes: String;
+    var res: String;
+
+    year = string.slice(0, 4);
+    month = string.slice(4, 6);
+    day = string.slice(6, 8);
+    hour = string.slice(9, 11);
+    minutes = string.slice(11, 13);
+
+    res = day + "/" + month + "/" + year + " de " + hour + ":" + minutes;
+
+    return res;
+  }
+  timeDisplayArrival(string) {
+    var day: String;
+    var month: String;
+    var year: String;
+    var hour: String;
+    var minutes: String;
+    var res: String;
+
+    year = string.slice(0, 4);
+    month = string.slice(4, 6);
+    day = string.slice(6, 8);
+    hour = string.slice(9, 11);
+    minutes = string.slice(11, 13);
+
+    res = /* day+"/"+month+"/"+year+ */" à " + hour + ":" + minutes;
+
+    return res;
+  }
+  dateDisplay(string) {
+    var day: String;
+    var month: String;
+    var year: String;
+    var res: String;
+
+    year = string.slice(0, 4);
+    month = string.slice(4, 6);
+    day = string.slice(6, 8);
+
+    res = day + "/" + month + "/" + year;
+
+    return res;
+  }
+
+  durationDisplay(departure, arrival) {
+    var hourDeparture: number;
+    var minutesDeparture: number;
+    var hourArrival: number;
+    var minutesArrival: number;
+
+    hourDeparture = parseInt(departure.slice(9, 11));
+    minutesDeparture = parseInt(departure.slice(11, 13));
+    hourArrival = parseInt(arrival.slice(9, 11));
+    minutesArrival = parseInt(arrival.slice(11, 13));
+
+
+    if (hourDeparture > hourArrival) {
+      if (minutesDeparture > minutesArrival) {
+
+        if (hourArrival + 23 - hourDeparture < 10) {
+
+          if (minutesArrival + 60 - minutesDeparture < 10) {
+            return ("0" + (hourArrival + 23 - hourDeparture) + ":0" + (minutesArrival + 60 - minutesDeparture));
+          } else {
+            return ("0" + (hourArrival + 23 - hourDeparture) + ":" + (minutesArrival + 60 - minutesDeparture));
+          }
+        } else {
+          if (minutesArrival + 60 - minutesDeparture < 10) {
+            return ((hourArrival + 23 - hourDeparture) + ":0" + (minutesArrival + 60 - minutesDeparture));
+          } else {
+            return ((hourArrival + 23 - hourDeparture) + ":" + (minutesArrival + 60 - minutesDeparture));
+          }
+        }
+
+      } else {
+        if (hourArrival + 24 - hourDeparture < 10) {
+
+          if (minutesArrival - minutesDeparture < 10) {
+            return ("0" + (hourArrival + 24 - hourDeparture) + ":0" + (minutesArrival - minutesDeparture));
+          } else {
+            return ("0" + (hourArrival + 24 - hourDeparture) + ":" + (minutesArrival - minutesDeparture));
+          }
+        } else {
+          if (minutesArrival + 60 - minutesDeparture < 10) {
+            return ((hourArrival + 24 - hourDeparture) + ":0" + (minutesArrival - minutesDeparture));
+          } else {
+            return ((hourArrival + 24 - hourDeparture) + ":" + (minutesArrival - minutesDeparture));
+          }
+        }
+        //return ((hourArrival+24-hourDeparture)+":"+(minutesArrival-minutesDeparture));
+      }
+    } else {
+      if (minutesDeparture > minutesArrival) {
+
+        if (hourArrival - 1 - hourDeparture < 10) {
+
+          if (minutesArrival + 60 - minutesDeparture < 10) {
+            return ("0" + (hourArrival - 1 - hourDeparture) + ":0" + (minutesArrival + 60 - minutesDeparture));
+          } else {
+            return ("0" + (hourArrival - 1 - hourDeparture) + ":" + (minutesArrival + 60 - minutesDeparture));
+          }
+        } else {
+          if (minutesArrival + 60 - minutesDeparture < 10) {
+            return ((hourArrival - 1 - hourDeparture) + ":0" + (minutesArrival + 60 - minutesDeparture));
+          } else {
+            return ((hourArrival - 1 - hourDeparture) + ":" + (minutesArrival + 60 - minutesDeparture));
+          }
+        }
+
+      } else {
+        if (hourArrival - hourDeparture < 10) {
+
+          if (minutesArrival - minutesDeparture < 10) {
+            return ("0" + (hourArrival - hourDeparture) + ":0" + (minutesArrival - minutesDeparture));
+          } else {
+            return ("0" + (hourArrival - hourDeparture) + ":" + (minutesArrival - minutesDeparture));
+          }
+        } else {
+          if (minutesArrival + 60 - minutesDeparture < 10) {
+            return ((hourArrival - hourDeparture) + ":0" + (minutesArrival - minutesDeparture));
+          } else {
+            return ((hourArrival - hourDeparture) + ":" + (minutesArrival - minutesDeparture));
+          }
+        }
+      }
+    }
+  }
+
+  TimeDisplay(string) {
+    var hour: String;
+    var minutes: String;
+    var res: String;
+
+    hour = string.slice(9, 11);
+    minutes = string.slice(11, 13);
+
+    res = hour + ":" + minutes;
+
+    return res;
+  }
+
+  startPoint(string){
+    for(var i = 0; i<string.length; i++){
+      if(string[i].display_informations != null){
+        return (this.TimeDisplay(string[i].base_departure_date_time)+ " - " + string[i].from.name);
+      }
+    }
+  }
+
+  stopPoint(string){
+    for(var i = string.length-1; i>=0; i--){
+      if(string[i].display_informations != null){
+        return (this.TimeDisplay(string[i].base_arrival_date_time)+ " - " + string[i].to.name);
+      }
+    }
+  }
+
+  changeCounter(string){
+    var counter = 0;
+    for(var i = 0; i<string.length; i++){
+      if(string[i].display_informations != null){
+        counter++;
+      }
+    }
+    if((counter-1)>0){
+      if((counter-1)>1){
+        return ((counter-1)+" changements");
+      } else {
+        return ((counter-1)+" changement");
+      }
+    } else {
+      return "trajet direct";
+    }
   }
 }
